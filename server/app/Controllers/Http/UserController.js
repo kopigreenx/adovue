3eaf101034302554b5b74ede21d0476ca3216ad7
Mokhamad Rizkianto <mokhamad.rizkianto@gmail.com>
2019-09-24 22:02:12 +0700

    initial commit

---
+++ server/app/Controllers/Http/UserController.js
@@ -0,0 +1,21 @@
+'use strict'
+const User = use('App/Models/User');
+class UserController {
+
+  async register({request}){
+    const {username,email,password} = request.all();
+    const user = await User.create({
+      username,email,password
+    });
+    return user;
+  }
+
+  async login({request,auth}){
+    const {username,password} = request.all();
+    const token = await auth.attempt({username,password});
+    return token;
+
+  }
+}
+
+module.exports = UserController
