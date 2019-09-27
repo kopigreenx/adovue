f1f3b79cd8e97af599461794c631fe12c3b321c2
kopigreenx <mokhamad.rizkianto@gmail.com>
2019-09-27 15:39:01 +0700

    client display and modal edit delete

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import * as riot from 'riot'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-//import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/jquery/dist/jquery.min.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import App from './app.riot'
 
